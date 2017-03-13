e6172b258af7cddb921930e1f714a4511c38b27a
Ryan Chanlatte <ryan.chanlatte@yahoo.com>
2017-03-13 08:57:57 -0400

    Amended committ

--- C_code/IQ_Array_Largest_Int_Sum_C.c
+++ C_code/IQ_Array_Largest_Int_Sum_C.c
@@ -3,12 +3,11 @@
 // Return the sum of the two largest integers in an array.
 int arraySum(int* numArray, int arraySize)
 {
-   int num1, num2, ans;
-   num1 = 0;
-   num2 = 0;
+   int ans, bufferArray[2] = {0, 0};
    // obvious fail condition
    ans = -1;
 
+   // if-guards
    if (arraySize <= 0)
    {
       printf("Array has no elements!\n");
@@ -23,30 +22,37 @@ int arraySum(int* numArray, int arraySize)
    {
       for (int i = 0; i < arraySize; i++)
       {
-         if (num1 < numArray[i])
+         if (bufferArray[0] < numArray[i])
          {
-            num1 = numArray[i];
-
-            if (num2 < num1)
-            {
-               num2 = (num1 - 1);
-            }
+            bufferArray[0] = numArray[i];
+            continue;
          }
       }
-      ans = num1 + num2;
+      for (int j = 0; j < arraySize; j++)
+      {
+         if (numArray[j] == bufferArray[0])
+         {
+            continue;
+         }
+         else if (bufferArray[1] < numArray[j])
+         {
+            bufferArray[1] = numArray[j];
+         }
+      }
+
+      ans = bufferArray[0] + bufferArray[1];
       return ans;
    }
 }
 
-
 int main(int argc, char* argv[])
 {
-   int numArray[10] = { 1,2,3,4,5,6,7,8,9,10 };
+   int numArray[10] = { 1,8,2,1,2,3,4,6,12,6 };
    int answer;
 
    answer = arraySum(numArray, 10);
 
-   // should be 19 as the two largest ints are 9 & 10.
+   // should be 18 as the two largest ints are 8 & 10.
    printf("\nThe sum of the largest integers in an array is: %i\n", answer);
    getchar();
 
